93f6318cdfd0e959baea57960d1094d530185df0
Natalia Sanchez <natita24@gmail.com>
2023-03-23 22:51:04 -0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,30 @@ import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ1i03UWaoufU-Q3W45bYEZuSBtGExnL8",
+  authDomain: "emociones-magicas-commerce.firebaseapp.com",
+  projectId: "emociones-magicas-commerce",
+  storageBucket: "emociones-magicas-commerce.appspot.com",
+  messagingSenderId: "124759839991",
+  appId: "1:124759839991:web:bac0fe475d236df642aff2",
+  measurementId: "G-NVX938KPR6"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
